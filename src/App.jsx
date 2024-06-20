76d95e7492dcd721d5d5db1d2619245eadf17b79
Avverma95 <ankit.verma@chplgroup.org>
2024-06-20 17:57:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FbLogin from "./Login";
-// import S3FileManager from "./S3FileManager";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/aws" element={<S3FileManager />} /> */}
+      
         <Route path="/" element={<FbLogin />} />
       </Routes>
     </Router>
